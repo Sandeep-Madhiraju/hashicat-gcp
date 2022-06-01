73f3ad2db3b8f553d2a2eb0a33bedd99dc220da0
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2022-06-01 21:56:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "google" {
 }
 
 resource "google_compute_network" "hashicat" {
-  name                    = "${var.prefix}-vpc-${var.region}"
+  name          = "${var.prefix}-vpc-${var.region}"
   auto_create_subnetworks = false
 }
 
