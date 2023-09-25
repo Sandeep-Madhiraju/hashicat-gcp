185725d7d0fca1e99843d96af34c6e3f3cbbbec1
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2023-09-25 17:50:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "google_compute_instance" "hashicat" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2304-lts"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
     }
   }
 
